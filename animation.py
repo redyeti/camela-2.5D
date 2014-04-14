5c215442cfebcb3256b7534c87a64066384860b9
Joshua Hopp <joshua.hopp@ims.uni-hannover.de>
2014-04-14 19:59:22 +0200

    improved animation api

--- animation.py
+++ animation.py
@@ -1,16 +1,31 @@
+from __future__ import division
 from collections import Iterator
 from priorityQueue import PriorityQueue
 
 class Animation(Iterator):
 	def __init__(self, app, length, fps):
+		self.__app = app
 		self.__events = PriorityQueue()
+		self.__fps = fps
+		self.__frame = 0
 
 	def __next__(self):
-		event = self.__events.pop().payload
+		# increase frame data
+		time, event = self.__events.pop()
+		self.__frame += 1
 
+		# update the queue
+		if event.maker is not None:
+			try:
+				e = event.maker.next()
+				self.__events.push(e)
+			if StopIteration:
+				pass
 
-		frame = 
+		return (self.__frame, time)
 
-		time = 
-
-		return (frame, time)
+	def addEvent(self, eventFn):
+		generator = eventFn(self, self.__app.camera, self.__app.canvas)
+		time = generator.next()
+		
+		self.__events.push(e
